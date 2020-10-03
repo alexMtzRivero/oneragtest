b920a27410beb5301ede20df26fe5814389abdeb
alex <alejandromartinez@uvicate.com>
2020-10-03 17:25:01 -0600

    itinerary model

--- Models/flight.py
+++ Models/flight.py
@@ -1,14 +1,13 @@
+from datetime import datetime
 class Flight():
 
     def __init__(self,source,destination,departure,arrival,flight_number,price,bags_allowed,bag_price):
         self.source = source
         self.destination = destination
-        self.departure = departure
-        self.arrival = arrival
+        self.departure = datetime.strptime(departure,"%Y-%m-%dT%H:%M:%S") 
+        self.arrival = datetime.strptime(arrival,"%Y-%m-%dT%H:%M:%S")
         self.flight_number = flight_number
-        self.price = price
-        self.bags_allowed = bags_allowed
-        self.bag_price = bag_price
+        self.price = float(price)
+        self.bags_allowed = int(bags_allowed)
+        self.bag_price = float(bag_price)
                 
-    def __str__(self):
-        return "personalizado"
---
+++ Models/itinerary.py
@@ -0,0 +1,31 @@
+from datetime import timedelta 
+class Itinerary():
+
+    def __init__(self,origin_code):
+        self.string_path = [origin_code]
+        self.path = []
+        self.flight_price = 0
+        self.bag_price = 0
+
+    def add(self, flight):
+        self.string_path.append(flight.destination)
+        self.path.append(flight)
+        self.flight_price += flight.price
+        self.bag_price += flight.bag_price
+
+    def is_on_time_for(departure_time,min_transfer, max_transfer):
+        last_arival = self.get_last_arival()
+        if last_arival is None:
+            return True
+        else:
+            dif = departure_time - last_arival
+            return dif >= timedelta(hours=min_transfer) and dif <= timedelta(hours=max_transfer)
+
+    def get_origin(self):
+        return self.string_path[0] 
+    
+    def get_last_arival(self):
+        return None if len(self.path) == 0 else self.path[-1].arrival
+    
+    def __str__(self):
+        return ','.join(self.string_path) #+ str(self.flight_price)
